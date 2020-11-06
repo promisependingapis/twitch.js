5afded307386afd627a6e6f7f87ee60633178b6e
Space_Inteprise <spaceinterpriseofficial@gmail.com>
2020-11-06 20:39:48 -0300

    Part 2

---
+++ src/index.js
@@ -0,0 +1,23 @@
+var Logger = require('./utils/logger');
+var Exited;
+
+// End of the program //
+process.stdin.resume();
+function exitHandler(options, exitCode) {
+    if (!Exited) {
+        if (exitCode === 'SIGINT') {
+            Logger.Warn('Manualy finished');
+        } else {
+            if ((exitCode || exitCode === 0) && !options.uncaughtException) Logger.Info('Program finished, code: ' + exitCode);
+            if ((exitCode || exitCode === 0) && options.uncaughtException) Logger.Fatal(exitCode);
+        }
+        Exited = true;
+    }
+    process.exit();
+}
+
+process.on('exit', exitHandler.bind(null,{}));
+process.on('SIGINT', exitHandler.bind(null, {}));
+process.on('SIGUSR1', exitHandler.bind(null, {}));
+process.on('SIGUSR2', exitHandler.bind(null, {}));
+process.on('uncaughtException', exitHandler.bind(null, {uncaughtException:true}));
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,31 +1,53 @@
-function colorParser(Text, color) {
-    
-}
-
-
+/* eslint-disable indent */
 var DateParser = require('./DateParser');
+var chalk = require('chalk');
 
-var debug = false;
+var debug = true;
 
 function log(Gravity) {
-    return function(message) {
-        console.log(`%c[${DateParser.getTime()}] `, 'color: #FF0000')
-    }
+    return function(Message) {
+        var Chalker;
+        var level = '';
+        var Msg = `[${DateParser.getTime()}] ${Gravity}: ${Message}`;
+        switch (Gravity) {
+            case 'Fatal':
+                Msg = `[${DateParser.getTime()}] ${Gravity} ${Message}`;
+                console.trace(chalk.bgWhite(chalk.red(Msg)));
+                process.exit(5);
+            break;
+            case 'Error':
+                level = 'error';
+                Chalker = chalk.red(Msg);
+            break;
+            case 'Warn':
+                level = 'warn';
+                Chalker = chalk.keyword('orange')(Msg);
+            break;
+            case 'Info':
+                level = 'log';
+                Msg = `${Gravity}: ${Message}`;
+                Chalker = chalk.blueBright(`[${DateParser.getTime()}] `) + Msg;
+            break;
+            case 'Debug':
+                level = 'debug';
+                Msg = `${Message}`;
+                Chalker = chalk.gray(`[${DateParser.getTime()}] ${Gravity}: `) + Msg;
+            break;
+        }
+        console[level](Chalker);
+    };
 }
 
 function logger(Gravity) {
-    if (debug && Gravity === 'Debug') {
-
-    } else {
-        if (Gravity !== 'Debug') {
-            return log(Gravity)
-        }
+    if ((Gravity === 'Debug' && debug === true) || (Gravity !== 'Debug')) {
+        return log(Gravity);
     }
 }
 
 module.exports = {
+    Fatal: logger('Fatal'),
     Error: logger('Error'),
     Warn: logger('Warn'),
     Info: logger('Info'),
     Debug: logger('Debug')
-}
+};
