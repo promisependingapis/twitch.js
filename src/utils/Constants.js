de8a984068c9ed648d400d5a2b91cb8418c10844
Lobo Metalúrgico <hugolino1000@gmail.com>
2020-11-07 17:08:30 -0300

    Default Options

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,5 +1,81 @@
 exports.Package = require('../../package.json');
 
+/**
+ * Options for a client.
+ * @typedef {Object} ClientOptions
+ * @property {string} [apiRequestMethod='sequential'] One of `sequential` or `burst`. The sequential handler executes
+ * all requests in the order they are triggered, whereas the burst handler runs multiple in parallel, and doesn't
+ * provide the guarantee of any particular order. Burst mode is more likely to hit a 429 ratelimit error by its nature,
+ * and is therefore slightly riskier to use.
+ * @property {number} [shardId=0] ID of the shard to run
+ * @property {number} [shardCount=0] Total number of shards
+ * @property {number} [messageCacheMaxSize=200] Maximum number of messages to cache per channel
+ * (-1 or Infinity for unlimited - don't do this without message sweeping, otherwise memory usage will climb
+ * indefinitely)
+ * @property {number} [messageCacheLifetime=0] How long a message should stay in the cache until it is considered
+ * sweepable (in seconds, 0 for forever)
+ * @property {number} [messageSweepInterval=0] How frequently to remove messages from the cache that are older than
+ * the message cache lifetime (in seconds, 0 for never)
+ * @property {boolean} [fetchAllMembers=false] Whether to cache all guild members and users upon startup, as well as
+ * upon joining a guild (should be avoided whenever possible)
+ * @property {boolean} [disableEveryone=false] Default value for {@link MessageOptions#disableEveryone}
+ * @property {boolean} [sync=false] Whether to periodically sync guilds (for user accounts)
+ * @property {number} [restWsBridgeTimeout=5000] Maximum time permitted between REST responses and their
+ * corresponding websocket events
+ * @property {number} [restTimeOffset=500] Extra time in millseconds to wait before continuing to make REST
+ * requests (higher values will reduce rate-limiting errors on bad connections)
+ * @property {number} [retryLimit=Infinity] How many times to retry on 5XX errors
+ * (Infinity for indefinite amount of retries)
+ * @property {WSEventType[]} [disabledEvents] An array of disabled websocket events. Events in this array will not be
+ * processed, potentially resulting in performance improvements for larger bots. Only disable events you are
+ * 100% certain you don't need, as many are important, but not obviously so. The safest one to disable with the
+ * most impact is typically `TYPING_START`.
+ * @property {WebsocketOptions} [ws] Options for the WebSocket
+ * @property {HTTPOptions} [http] HTTP options
+ */
+exports.DefaultOptions = {
+    apiRequestMethod: 'sequential',
+    messageCacheMaxSize: 200,
+    messageCacheLifetime: 0,
+    messageSweepInterval: 0,
+    sync: false,
+    restWsBridgeTimeout: 5000,
+    retryLimit: Infinity,
+    disabledEvents: [],
+    restTimeOffset: 500,
+  
+    /**
+     * WebSocket options (these are left as snake_case to match the API)
+     * @typedef {Object} WebsocketOptions
+     * @property {number} [large_threshold=250] Number of members in a guild to be considered large
+     * @property {boolean} [compress=true] Whether to compress data sent on the connection
+     * (defaults to `false` for browsers)
+     */
+    ws: {
+        large_threshold: 250,
+        compress: require('os').platform() !== 'browser',
+        properties: {
+            $os: process ? process.platform : 'twitch.js',
+            $browser: 'twitch.js',
+            $device: 'twitch.js',
+            $referrer: '',
+            $referring_domain: '',
+        },
+        version: 6,
+    },
+  
+    /**
+     * HTTP options
+     * @typedef {Object} HTTPOptions
+     * @property {number} [version=7] API version to use
+     * @property {string} [api='http://irc-ws.chat.twitch.tv'] Base url of the API
+     */
+    http: {
+        version: 7,
+        host: 'http://irc-ws.chat.twitch.tv'
+    },
+};
+
 exports.Errors = {
     INVALID_RATE_LIMIT_METHOD: 'Unknown rate limiting method.',
     INVALID_TOKEN: 'An invalid token was provided.'
