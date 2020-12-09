fe8e835966ce6421006c083a15661656dbd635ea
Space_Inteprise <indtekworldbaj99@gmail.com>
2020-12-08 21:35:43 -0300

    Fix comments

--- src/utils/util.js
+++ src/utils/util.js
@@ -3,12 +3,12 @@ class util {
         throw new Error(`The ${this.constructor.name} class may not be instantiated.`);
     }
     /**
-   * Sets default properties on an object that aren't already specified.
-   * @param {Object} def Default properties
-   * @param {Object} given Object to assign defaults to
-   * @returns {Object}
-   * @private
-   */
+     * Sets default properties on an object that aren't already specified.
+     * @param {Object} def Default properties
+     * @param {Object} given Object to assign defaults to
+     * @returns {Object}
+     * @private
+     */
     static mergeDefault(def, given) {
         if (!given) return def;
         for (const key in def) {
