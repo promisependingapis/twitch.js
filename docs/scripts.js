d02705abf55edda567d48c9f78d617161e66f9ef
Space_Inteprise <spaceinterpriseofficial@gmail.com>
2021-01-10 18:41:00 -0300

    Viwer

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,9 +1,10 @@
 import { request } from "https://cdn.skypack.dev/@octokit/request";
 var xhr = new XMLHttpRequest();
 xhr.onload = async () => {
-    await request('POST /markdown', {
+    const markdown = await request('POST /markdown', {
         text: xhr.response
     });
+    document.getElementsByClassName('File-viewer').innerHTML = markdown;
 };
 xhr.open('get', "./README.md");
 xhr.send();
