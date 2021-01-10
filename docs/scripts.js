4e3e09773ceb42fc13a4010b5d1ead1c1cbb703b
Space_Inteprise <spaceinterpriseofficial@gmail.com>
2021-01-10 18:26:07 -0300

    FOR GOD PLEASE WORK

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,7 +1,6 @@
-import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
 var xhr = new XMLHttpRequest();
-xhr.onload = () => {
-    Octokit.request('POST /markdown', {
+xhr.onload = async () => {
+    await Octokit.request('POST /markdown', {
         text: xhr.response
     });
 };
