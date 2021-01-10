d6b1d94f4f064fcac57b43461319eae8e4c94eae
Space_Inteprise <spaceinterpriseofficial@gmail.com>
2021-01-10 15:08:42 -0300

    Aprimorate layout

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+document.getElementsByClassName('Content')[0].style.marginTop = ((3.5/100)*window.innerWidth) + 'px';
+document.getElementsByClassName('Content')[0].style.height = window.innerHeight - ((3.5/100)*window.innerWidth) + 'px';
+
 // https://stackoverflow.com/questions/247483/http-get-request-in-javascript
 function httpGetAsync(theUrl, callback) {
     var xmlHttp = new XMLHttpRequest();
