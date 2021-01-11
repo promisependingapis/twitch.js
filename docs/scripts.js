440db706c40cd73010b01fee0429e065ac9c13e4
Space_Inteprise <spaceinterpriseofficial@gmail.com>
2021-01-11 12:29:07 -0300

    Update scripts.js

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,8 +1,5 @@
 document.getElementsByClassName('File-Viewer')[0].style.marginTop = ((3.5/100)*window.innerWidth) + 'px';
 document.getElementsByClassName('File-Viewer')[0].style.height = window.innerHeight - ((3.5/100)*window.innerWidth) + 'px';
-setTimeout(() => {
-    document.getElementsByTagName('body')[0].style.height = document.getElementsByTagName('html')[0].scrollHeight + ((3.5/100)*window.innerWidth) + 'px';
-}, 300);
 window.onresize = () => {
     document.getElementsByClassName('File-Viewer')[0].style.marginTop = ((3.5/100)*window.innerWidth) + 'px';
     document.getElementsByClassName('File-Viewer')[0].style.height = window.innerHeight - ((3.5/100)*window.innerWidth) + 'px';
