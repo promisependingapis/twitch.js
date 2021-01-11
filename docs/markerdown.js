2e139efe82cb1cdc4e6412eb9b5941b44917b155
Space_Inteprise <spaceinterpriseofficial@gmail.com>
2021-01-10 21:56:06 -0300

    Fix layouts

--- docs/markerdown.js
+++ docs/markerdown.js
@@ -9,7 +9,14 @@ xhr.onload = async () => {
     document.getElementsByClassName('File-Viewer')[0].appendChild(htmlObject);
     [...document.getElementsByClassName('highlight')].forEach((element) => {
         hljs.highlightBlock(element);
-    })
+        element.style.borderRadius = '1.5vw';
+        element.children[0].style.margin = '0';
+        element.children[0].style.padding = '1vw';
+    });
+    [...document.getElementsByTagName('li')].forEach((element) => {
+        element.innerHTML = element.innerHTML.replaceAll('[ ]', '<input type="checkbox" disabled>');
+        element.innerHTML = element.innerHTML.replaceAll('[X]', '<input type="checkbox" disabled checked>');
+    });
 };
 xhr.open('get', "./README.md");
 xhr.send();
--- docs/scripts.js
+++ docs/scripts.js
@@ -1,16 +1,9 @@
 document.getElementsByClassName('File-Viewer')[0].style.marginTop = ((3.5/100)*window.innerWidth) + 'px';
 document.getElementsByClassName('File-Viewer')[0].style.height = window.innerHeight - ((3.5/100)*window.innerWidth) + 'px';
-document.getElementsByTagName('html')[0].style.height = document.getElementsByTagName('html')[0].scrollHeight + ((3.5/100)*window.innerWidth) + 'px';
 setTimeout(() => {
     document.getElementsByTagName('body')[0].style.height = document.getElementsByTagName('html')[0].scrollHeight + ((3.5/100)*window.innerWidth) + 'px';
 }, 300);
 window.onresize = () => {
     document.getElementsByClassName('File-Viewer')[0].style.marginTop = ((3.5/100)*window.innerWidth) + 'px';
     document.getElementsByClassName('File-Viewer')[0].style.height = window.innerHeight - ((3.5/100)*window.innerWidth) + 'px';
-    document.getElementsByTagName('html')[0].style.height = document.getElementsByTagName('html')[0].scrollHeight + ((3.5/100)*window.innerWidth) + 'px';
 }
-
-[...document.getElementsByTagName('li')].forEach((element) => {
-	element.innerHTML = element.innerHTML.replaceAll('[ ]', '<input type="checkbox" disabled>');
-	element.innerHTML = element.innerHTML.replaceAll('[X]', '<input type="checkbox" disabled checked>');
-});
