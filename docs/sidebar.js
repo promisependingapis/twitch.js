9750d9b62b018a9847cf2217aae8e711326afa93
Space_Inteprise <spaceinterpriseofficial@gmail.com>
2021-01-17 22:40:12 -0300

    Update sidebar.js

--- docs/sidebar.js
+++ docs/sidebar.js
@@ -2,12 +2,14 @@ var xhra = new XMLHttpRequest();
 xhra.onload = async () => {
     const json = JSON.parse(xhra.response);
     json.classes.forEach((classValue) => {
-        const br = document.createElement('br');
-        const button = document.createElement('a');
-        button.innerText = classValue.name;
-        button.href = '#' + classValue.name;
-        document.getElementsByClassName('SideBarClasses')[0].appendChild(button);
-        document.getElementsByClassName('SideBarClasses')[0].appendChild(br);
+        if (classValue.access !== 'private') {
+            const br = document.createElement('br');
+            const button = document.createElement('a');
+            button.innerText = classValue.name;
+            button.href = '#' + classValue.name;
+            document.getElementsByClassName('SideBarClasses')[0].appendChild(button);
+            document.getElementsByClassName('SideBarClasses')[0].appendChild(br);
+        }
     });
     json.typedefs.forEach((typedefValue) => {
         const br = document.createElement('br');
