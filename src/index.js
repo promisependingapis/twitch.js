967dc3387c28f8ac6d39c92ae2819fc1fc3b476f
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2021-03-22 22:40:36 -0300

    Make Collection Public + update dependencies

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ module.exports = {
     // Main
     Client: require(path.resolve(__dirname,'client','Client')),
     // Utils
+    Collection: require(path.resolve(__dirname,'utils','collection')),
     logger: require(path.resolve(__dirname,'utils','logger')),
+    
 };
