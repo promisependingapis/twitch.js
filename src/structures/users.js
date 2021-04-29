6fb74036642b0297f8ae6f65c77c5a205e2ab3de
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2021-04-29 00:31:14 -0300

    Add ban command

--- src/structures/users.js
+++ src/structures/users.js
@@ -38,6 +38,13 @@ class Users {
         this.broadcaster = data.broadcaster ? data.broadcaster : false;
     }
 
+    /**
+     * Timeout user into a channel
+     * @param {*} channel 
+     * @param {*} secounds 
+     * @param {*} reason 
+     * @returns Promise<Any>
+     */
     timeout(channel, secounds, reason) {
         if (typeof(channel) === 'number') {
             reason = secounds;
@@ -46,6 +53,30 @@ class Users {
         return this.client.sleept.methods.sendMessage(
             (this.channel ? (typeof(this.channel) === 'string' ? this.channel : this.channel.name) : channel), 
             '/timeout ' + this.userName + ' ' + secounds + ' ' + reason);
+    }
+
+    /**
+     * Ban user from an channel
+     * @param {*} channel 
+     * @param {*} reason 
+     * @returns Promise<Any>
+     */
+    ban(channel, reason) {
+        if (!channel || !(channel instanceof Array)) {
+            if (!reason) {
+                reason = channel;
+            }
+
+            if (!reason) reason = '';
+
+            return this.client.sleept.methods.sendMessage(channel, `/ban ${this.username} ${reason}`);
+        } else {
+            if (!reason) reason = '';
+
+            channel.forEach((element) => {
+                this.client.sleept.methods.sendMessage(element, `/ban ${this.username} ${reason}`);
+            });
+        }
     }
 }
 
