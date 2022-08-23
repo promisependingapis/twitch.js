3e5cc6a46bf523149e25f1373d74c9749dd2492b
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2022-08-23 14:07:19 -0300

    (fix): remove build folder

--- build/main.d.ts
+++
@@ -1,4 +0,0 @@
-export * as Interfaces from './interfaces';
-export * as Utils from './utils';
-export * from './client';
-//# sourceMappingURL=main.d.ts.map
--- build/utils/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './logger';
-export * as waiters from './waiters';
-export * as parser from './messageParser';
-export * as autoLogEnd from './autoLogEnd';
-//# sourceMappingURL=index.d.ts.map
