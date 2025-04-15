8f72bbdb423e018c6485482e0f4d6613606d84a8
Space_Fox <44732812+SpaceFox1@users.noreply.github.com>
2025-04-14 23:17:15 -0300

    (refactor): Docs v3-beta
    Co-authored-by: Lobo Metalúrgico <LoboMetalurgico@users.noreply.github.com>

--- scripts.js
+++ scripts.js
@@ -15,20 +15,17 @@ httpGetAsync('https://api.npmjs.org/downloads/range/2013-08-21:2100-08-21/@twitc
     response.downloads.forEach((download) => {
         totalDownloads += download.downloads;
     })
-    document.getElementsByClassName('TotalDownloads')[0].innerText = totalDownloads.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + ' downloads on npm';
     var TextArray = document.getElementsByClassName('FooterStatistics')[0].innerText.toString().split('Loading');
     TextArray.shift();
     document.getElementsByClassName('FooterStatistics')[0].innerText = totalDownloads.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + TextArray.join('Loading');
 
     httpGetAsync('https://api.github.com/repos/twitchapis/twitch.js', (json) => {
         var response = JSON.parse(json);
-        document.getElementsByClassName('TotalStars')[0].innerText = response.stargazers_count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + ' stars on Github';
         var TextArray = document.getElementsByClassName('FooterStatistics')[0].innerText.toString().split('Loading');
         document.getElementsByClassName('FooterStatistics')[0].innerText = TextArray[0] + response.stargazers_count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + ' stars, Loading ' + TextArray[2];
 
         httpGetAsync('https://api.github.com/repos/twitchapis/twitch.js/stats/contributors', (json) => {
             var response = JSON.parse(json);
-            document.getElementsByClassName('TotalContributors')[0].innerText = response.length.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + ' contributors on Github'
             var TextArray = document.getElementsByClassName('FooterStatistics')[0].innerText.toString().split('Loading');
             document.getElementsByClassName('FooterStatistics')[0].innerText = TextArray[0] + response.length.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + ' contributors';
         });
@@ -51,20 +48,4 @@ function CopyToClipboard(ElementName) {
             copyText.onclick = ()=>{CopyToClipboard(ElementName, fallbackColor)};
         }, 1500);
     });
-} 
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.remove('hiddenTransition');
-            entry.target.classList.add('showTransition');
-        } else {
-            entry.target.classList.remove('showTransition');
-            entry.target.classList.add('hiddenTransition');
-        }
-    })
-})
-
-const transitionElements = [...document.getElementsByClassName('appearToggle')];
-
-transitionElements.forEach((he) => observer.observe(he));
+}
